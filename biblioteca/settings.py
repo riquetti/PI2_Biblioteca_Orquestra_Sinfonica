b4aa2dc3a3c1b24f65608b35d5fea646169807dc
riquetti <lfriquetti@hotmail.com>
2024-10-29 10:21:46 -0300

    Projeto Integrador 2
    Deploy - teste 5

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -163,4 +163,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
