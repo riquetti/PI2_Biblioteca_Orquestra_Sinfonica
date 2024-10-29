84023c19a3f60452ac191cbcd6b6d681a50546fb
riquetti <lfriquetti@hotmail.com>
2024-10-29 10:33:03 -0300

    Deploy Vercel

--- biblioteca/wsgi.py
+++ biblioteca/wsgi.py
@@ -8,11 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
-
+django.setup()
 application = get_wsgi_application()
 
 app = application
